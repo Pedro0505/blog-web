1fb98e8006a2d1357344c4f6590432579cc9c8f6
pedro <pedrohrds05@hotmail.com>
2023-06-06 16:13:26 -0300

    fix: fixing type of return in getPosts

--- src/api/getPosts.ts
+++ src/api/getPosts.ts
@@ -1,7 +1,7 @@
 import handlesAxios from './handleAxios';
 import IPosts from './interfaces/IPosts';
 
-const getPosts = async (): Promise<IPosts> => {
+const getPosts = async (): Promise<IPosts[]> => {
   const response = await handlesAxios.get('/posts');
 
   return response.data;
