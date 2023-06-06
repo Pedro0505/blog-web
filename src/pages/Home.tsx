ff6bb21ab1cf3231801262d1a857e15de1431ac7
pedro <pedrohrds05@hotmail.com>
2023-06-06 19:50:49 -0300

    feat: adding posts cards componnt in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Header from '../components/header/Header';
+import { PostsContext } from '../context/PostsContext';
+import PostsCards from '../components/postsCards/PostsCards';
 
 function Home() {
+  const { fetchPosts, posts } = useContext(PostsContext);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <div>
       <Header />
+      {
+        posts.map((e) => (
+          <PostsCards
+            key={e.id}
+            category={e.category}
+            description={e.description}
+            published={e.published}
+            title={e.title}
+          />
+        ))
+      }
     </div>
   );
 }
