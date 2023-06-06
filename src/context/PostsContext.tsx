7aa119488127275593d143449ca930695b9a5e02
pedro <pedrohrds05@hotmail.com>
2023-06-06 16:12:37 -0300

    feat: create context to posts

---
+++ src/context/PostsContext.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useState } from 'react';
+import IPostsContextProps from './interfaces/IPostsContextProps';
+import IPostsContextType from './interfaces/IPostsContextType';
+import getPosts from '../api/getPosts';
+
+const initialValue = {
+  posts: [{
+    id: 1,
+    title: '',
+    description: '',
+    content: '',
+    published: new Date('2011-08-01T19:58:00.000Z'),
+    category: '',
+  }],
+  fetchPosts: async () => {},
+};
+
+export const PostsContext = createContext<IPostsContextType>(initialValue);
+
+export const PostsProvider = ({ children }: IPostsContextProps) => {
+  const [posts, setPosts] = useState(initialValue.posts);
+
+  const fetchPosts = async () => {
+    const response = await getPosts();
+
+    setPosts(response);
+  };
+
+  const context = {
+    posts,
+    fetchPosts,
+  };
+
+  return (
+    <PostsContext.Provider value={ context }>
+      { children }
+    </PostsContext.Provider>
+  );
+};
---
+++ src/context/interfaces/IPostsContextProps.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+
+interface IPostsContextProps {
+  children: ReactNode
+}
+
+export default IPostsContextProps;
