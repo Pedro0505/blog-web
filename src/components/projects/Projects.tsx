9e9d6cdd1038abba09e593c5269e3a53e3d873bb
pedro <pedrohrds05@hotmail.com>
2023-06-09 09:24:00 -0300

    feat: create projects component

---
+++ src/components/projects/Projects.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+function Projects() {
+  return (
+    <article className="projects-container">
+    </article>
+  );
+}
+
+export default Projects;
