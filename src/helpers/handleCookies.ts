fe95367ddc269c7f3b03daffd6cd98d37f76dbfb
pedro <pedrohrds05@hotmail.com>
2023-07-10 09:46:22 -0300

    feat: adding a function to get cookie by key

--- src/helpers/handleCookies.ts
+++ src/helpers/handleCookies.ts
@@ -6,6 +6,8 @@ export const setCookie = (key: string, data: string, options?: CookieSetOptions)
   cookie.set(key, data, options);
 };
 
+export const getCookie = (key: string) => cookie.get(key);
+
 export const removeCookie = (key: string) => {
   cookie.remove(key);
 };
