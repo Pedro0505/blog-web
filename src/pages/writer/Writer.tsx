93352f74c0db8bddd04dfae6838422c7f4650a0f
pedro <pedrohrds05@hotmail.com>
2023-07-24 08:35:31 -0300

    feat: adding writer page

--- src/pages/writer/Writer.tsx
+++ src/pages/writer/Writer.tsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import useTokenRedirect from '../../hooks/useTokenRedirect';
+import CreatePost from '../../components/createPost/CreatePost';
+import './style.css';
 
 function Writer() {
   const authorized = useTokenRedirect();
-  const [loged, setLoged] = useState(false);
+  const [logged, setLogged] = useState(false);
 
   useEffect(() => {
-    setLoged(authorized.current);
+    setLogged(authorized.current);
   }, [authorized.current]);
 
   return (
     <>
       {
-        loged && <div>Writer</div>
+        logged && (
+          <main className="writer-content">
+            <CreatePost />
+          </main>
+        )
       }
     </>
   );
