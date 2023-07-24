ee0ce895f4253acf58e31c6201094b2b75ca0317
pedro <pedrohrds05@hotmail.com>
2023-07-24 08:34:56 -0300

    feat: creating api call to create Post

---
+++ src/api/createPost.ts
@@ -0,0 +1,15 @@
+import CookieKeys from '../constants/CookieKeys';
+import { getCookie } from '../helpers/handleCookies';
+import handlesAxios from './handleAxios';
+import IPostCreate from './interfaces/IPostCreate';
+import IPosts from './interfaces/IPosts';
+
+const createPost = async (post: IPostCreate): Promise<IPosts> => {
+  const token = getCookie(CookieKeys.SessionKey);
+
+  const response = await handlesAxios.post('/posts', post, { headers: { Authorization: token } });
+
+  return response.data;
+};
+
+export default createPost;
---
+++ src/api/interfaces/IPostCreate.ts
@@ -0,0 +1,8 @@
+interface IPostCreate {
+  title: string;
+  description: string;
+  content: string;
+  category: string;
+}
+
+export default IPostCreate;
